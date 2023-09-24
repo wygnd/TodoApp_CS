8a92eafba5c894587bd452c63edec8bdbc61ed2b
Denis <netfixk@yandex.ru>
2023-09-24 12:28:28 +0300

    init app

---
+++ TodoApp/Models/TodoModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoApp.Models
+{
+    internal class TodoModel: INotifyPropertyChanged
+    {
+        private string _text;
+        private bool _isDone;
+
+        public DateTime CreationDate { get; set; } = DateTime.Now;
+
+		public bool IsDone
+		{
+			get { return _isDone; }
+			set 
+			{
+				if (_isDone == value) return;
+				_isDone = value;
+				OnPropertyChanged("IsDone");
+
+            }
+		}
+
+		public string Text
+		{
+			get { return _text; }
+			set 
+			{
+				if(_text == value) return;
+				_text = value;
+				OnPropertyChanged("Text");
+            }
+		}
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+		protected virtual void OnPropertyChanged(string properyName = "")
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(properyName));
+		}
+    }
+}
